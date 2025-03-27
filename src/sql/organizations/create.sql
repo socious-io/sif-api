fe75f583b3787fff970e9a1eb89bf6ed5fc31a5b
Ehsan Mahmoudi <me@e-mahmoudi.me>
2025-03-27 16:47:32 +0330

    fix user_voted flag

--- src/sql/organizations/create.sql
+++ src/sql/organizations/create.sql
@@ -21,9 +21,12 @@ DO UPDATE SET
     website = EXCLUDED.website,
     mission = EXCLUDED.mission,
     culture = EXCLUDED.culture,
-    status = EXCLUDED.status,
-    verified_impact = EXCLUDED.verified_impact,
-    verified = EXCLUDED.verified,
+    status = CASE
+        WHEN EXCLUDED.status = 'ACTIVE' THEN organizations.status
+        ELSE EXCLUDED.status
+    END,
+    verified_impact = CASE WHEN EXCLUDED.verified_impact THEN EXCLUDED.verified_impact ELSE organizations.verified_impact END,
+    verified = CASE WHEN EXCLUDED.verified THEN EXCLUDED.verified ELSE organizations.verified END,
     cover=EXCLUDED.cover,
     logo=EXCLUDED.logo
 RETURNING id;
