d3cc88e2f15bc9c455a33cb926e0dd33834c74ba
Mohammad Hosein Balkhani <mohammadhb25@gmail.com>
2025-09-09 22:50:09 +0330

    feat: add deleted user (#50)
    refactor: sync identities through nats
    feat: add worker
    feat: add workers

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,47 +1,33 @@
 package main
 
 import (
-	"log"
-	"sif/src/apps"
+	"sif/src/apps/workers"
 	"sif/src/config"
 	"time"
 
-	"github.com/socious-io/goaccount"
 	"github.com/socious-io/gomq"
-	"github.com/socious-io/gopay"
 	database "github.com/socious-io/pkg_database"
 )
 
 func main() {
+
 	config.Init("config.yml")
 	database.Connect(&database.ConnectOption{
 		URL:         config.Config.Database.URL,
 		SqlDir:      config.Config.Database.SqlDir,
-		MaxRequests: 50,
+		MaxRequests: 5,
 		Interval:    30 * time.Second,
 		Timeout:     5 * time.Second,
 	})
 
-	if err := gopay.Setup(gopay.Config{
-		DB:     database.GetDB(),
-		Prefix: "gopay",
-		Chains: config.Config.Payment.Chains,
-		Fiats:  config.Config.Payment.Fiats,
-	}); err != nil {
-		log.Fatalf("gopay error %v", err)
-	}
-
-	//Initializing GoMQ Library
 	gomq.Setup(gomq.Config{
 		Url:        config.Config.Nats.Url,
 		Token:      config.Config.Nats.Token,
 		ChannelDir: "fund",
+		Consumers:  map[string]func(interface{}){},
 	})
-	gomq.Connect()
 
-	if err := goaccount.Setup(config.Config.GoAccounts); err != nil {
-		log.Fatalf("goaccount error %v", err)
-	}
+	workers.RegisterConsumers()
 
-	apps.Serve()
+	gomq.Init()
 }
